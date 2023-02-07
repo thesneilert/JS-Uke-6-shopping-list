5e370a0452664a2df060280a1aac423f62b3afd6
thesneilert <eilert.gaming@outlook.com>
2023-02-07 12:28:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,45 +2,18 @@
 
 const app = document.getElementById("app"); //henter div med id "app"
 
-let inputValue = ''
-let handleliste = ['Melk', 'Bakepulver', 'Kakao', 'Egg', 'Smør']
-
 
 // ----- VIEW -----
 
-show();
-function show() {
-    let html = '';
+updateView()
 
-    for (let i = 0; i < handleliste.length; i++) {
-        html += `
-            <li>
-                ${handleliste[i]} 
-                <button onclick="changeItem(${i})">Changes</button>
-                <button onclick="deleteItem(${i})">x</button>
-            </li> `
-    }
-
-    html += `<input oninput="inputValue = this.value" type="text"> <button onclick="addItem()">Legg til </button>`;
-    document.getElementById("app").innerHTML = html;
+function updateView() {
+    document.getElementById("app").innerHTML = `
+        Text i html her
+        `;
 }
+
 
 
 // ----- CONTROLLER -----
 
-show();
-function show() {
-    let html = '';
-
-    for (let i = 0; i < handleliste.length; i++) {
-        html += `
-            <li>
-                ${handleliste[i]} 
-                <button onclick="changeItem(${i})">Changes</button>
-                <button onclick="deleteItem(${i})">x</button>
-            </li> `
-    }
-
-    html += `<input oninput="inputValue = this.value" type="text"> <button onclick="addItem()">Legg til </button>`;
-    document.getElementById("app").innerHTML = html;
-}
