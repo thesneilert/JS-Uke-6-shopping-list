d3759df17bac9e164dadbd03c9699bfe4e6b0599
thesneilert <eilert.gaming@outlook.com>
2023-02-08 10:11:27 +0100

    Inital Commit 2

--- script.js
+++ script.js
@@ -2,18 +2,42 @@
 
 const app = document.getElementById("app"); //henter div med id "app"
 
+let inputValue = ''
+let handleliste = ['']
 
 // ----- VIEW -----
 
-updateView()
+show();
+function show() {
+    let html = '';
 
-function updateView() {
-    document.getElementById("app").innerHTML = `
-        Text i html her
-        `;
+    for (let i = 0; i < handleliste.length; i++) {
+        html += `
+            <li>
+                ${handleliste[i]} 
+                <button onclick="changeItem(${i})">Changes</button>
+                <button onclick="deleteItem(${i})">x</button>
+            </li> `
+    }
+
+    html += `<br><input oninput="inputValue = this.value" type="text"> <button onclick="addItem()">Legg til </button>`;
+    document.getElementById("app").innerHTML = html;
 }
-
 
 
 // ----- CONTROLLER -----
 
+function addItem() {
+    handleliste.push(inputValue);
+    inputValue = "";
+    show();
+}
+
+function deleteItem(index) {
+    handleliste.splice(index, 1);
+    show();
+}
+function changeItem(index) {
+    handleliste[index] = inputValue;
+    show();
+}
